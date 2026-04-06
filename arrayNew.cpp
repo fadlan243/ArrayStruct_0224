97c89cc1b35c03ca8464d84b5f2c15b8b4eed5a0
fadlan243 <fadlan.nasrizal17@sma.belajar.id>
2026-04-06 19:40:59 +0700

    Menampilkan dengan menggunakan perulangan for

--- arrayNew.cpp
+++ arrayNew.cpp
@@ -38,5 +38,9 @@ int main() {
         cin >> kamu[i];
     }
     cout << endl;
-    
+    // Menampilkan dengan menggunakan perulangan for (dari gambar)
+    for (int i = 0; i <= 4; i++)
+    {
+        cout << "Data ke-" << i+1 << " = " << kamu[i] << endl;
+    }
 }
