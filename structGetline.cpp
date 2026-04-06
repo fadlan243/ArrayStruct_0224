82032978d777437d8d60945e8a08fc22253c984a
fadlan243 <fadlan.nasrizal17@sma.belajar.id>
2026-04-06 19:45:05 +0700

    input data orang

--- structGetline.cpp
+++ structGetline.cpp
@@ -17,5 +17,18 @@ struct orang
 
 
 int main() {
+    orang mhs;
+    // mengisi data
+    cout << "Mengisi Data" << endl;
+    cout << "Nama : ";
+    cin >> mhs.nama;
+    cout << "Desa : ";
+    cin >> mhs.alamat.desa;
+    cout << "Kota : ";
+    cin >> mhs.alamat.kota;
+    cout << "Umur : ";
+    cin >> mhs.umur;
+    cout << endl;
+
 
 }
