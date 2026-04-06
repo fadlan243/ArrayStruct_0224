e1816faf8f58635efbfd43466c56f266dcfa9ed4
fadlan243 <fadlan.nasrizal17@sma.belajar.id>
2026-04-06 19:35:24 +0700

    first commit

---
+++ getline.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+#include <string>
+
+struct detailAlamat 
+{
+    string desa;
+    string kota;
+};
+
+struct orang 
+{
+    string nama;
+    detailAlamat alamat;
+    int umur;
+};
+
+int main() {
+    orang mhs;
+    // mengisi data
+    cout << "Nama : ";
+    getline(cin, mhs.nama);
+    cout << "Desa : ";
+    getline(cin, mhs.alamat.desa);
+    cout << "Kota : ";
+    getline(cin, mhs.alamat.kota);
+    cout << "Umur : ";
+    cin >> mhs.umur;
+    cout << endl;
+
+    // menampilkan data
+    cout << "Menampilkan Data" << endl;
+    cout << "Nama : " << mhs.nama << endl;
+    cout << "Alamat : " << mhs.alamat.desa << ", " << mhs.alamat.kota << endl;
+    cout << "Umur : " << mhs.umur << endl;
+ 
+}
---
+++ structGetline.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct detailAlamat 
+{
+    string desa;
+    string kota;
+};
+
+struct orang 
+{
+    string nama;
+    detailAlamat alamat;
+    int umur;
+};
+
+
+int main() {
+    orang mhs;
+    // mengisi data
+    cout << "Mengisi Data" << endl;
+    cout << "Nama : ";
+    cin >> mhs.nama;
+    cout << "Desa : ";
+    cin >> mhs.alamat.desa;
+    cout << "Kota : ";
+    cin >> mhs.alamat.kota;
+    cout << "Umur : ";
+    cin >> mhs.umur;
+    cout << endl;
+
+    // menampilkan data
+    cout << "Menampilkan Data" << endl;
+    cout << "Nama : " << mhs.nama << endl;
+    cout << "Alamat : " << mhs.alamat.desa << ", " << mhs.alamat.kota << endl;
+    cout << "Umur : " << mhs.umur << endl;
+ 
+}
