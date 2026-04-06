2140f68aff95cc7a90bf29ccf08fa404f0247a7d
fadlan243 <fadlan.nasrizal17@sma.belajar.id>
2026-04-06 19:47:39 +0700

    output data

--- arrayStruct.cpp
+++ arrayStruct.cpp
@@ -42,6 +42,16 @@ int main() {
         cout << endl;
     }
 
+    // output data
+    for (int i = 0; i <= 2; i++)
+    {
+        cout << "Data Penduduk ke-" << i + 1 << endl;
+        cout << "Nama : " << mhs[i].nama << endl;
+        cout << "Desa : " << mhs[i].alamat.desa << endl;
+        cout << "Kota : " << mhs[i].alamat.kota << endl;
+        cout << "Umur : " << mhs[i].umur << endl;
+        cout << endl;
+    }
 
     return 0;
 }
